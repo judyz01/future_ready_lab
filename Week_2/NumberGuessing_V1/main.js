633c2ca5c8439f30edede9643a29306b941b3c91
judyz01 <judy.j.zhang@gmail.com>
2018-06-26 11:55:54 -0700

    Adjustments made to Number Guessing Game

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,20 +1,38 @@
 /*eslint-env browser*/
 function playGame (){
 alert("First enter a low number, then a high number. Then, guess a random number between them.");
-
+    
 //get the low and high bounds
 //uses parseInt() to make sure we have numbers not strings
+// isNaN prevents a letter from being typed in
 var from = parseInt(prompt("Enter the lower bound"));
-
-var to = parseInt(prompt ("Enter the higher bound"));
+    while(from<0 || from>1000 || isNaN(from)){
+        from = parseInt(prompt("Enter a NUMBER higher than 0 and lower than 1000."));
+    }   
+    
+var to = parseInt(prompt("Enter the higher bound"));
+    while(to<0 || to>1000 || isNaN(to)){
+        to = parseInt(prompt("Enter a NUMBER higher than 0 and lower than 1000."));
+    }
+       
+//Makes sure that "to" is greater than "from"
+ while (to<=from){
+            to = parseInt(prompt("Please enter a number greater than " + from));
+        }
 
 //get an integer between [from,to]
 //Math.random() returns decimals, used Math.round to get whole number
 var target = Math.round(Math.random() * (to-from) + from);
 
-var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
+    var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
 
-var totalGuesses = 1; 
+    var totalGuesses = 1; 
+    
+//makes sure that the NUMBER guessed is within "from" and "to"
+//makes sure a number is typed in
+while(currentGuess<from || currentGuess>to || isNaN(currentGuess)){
+    currentGuess = parseInt(prompt("Please type in a NUMBER between "+ from + " and " + to));
+}
 
 //loop until user guesses correct number
 while(currentGuess != target){
@@ -25,8 +43,7 @@ while(currentGuess != target){
     else if (currentGuess > target){
     currentGuess = parseInt(prompt("Enter a lower number"));
     totalGuesses++;
-    }
-    
+    }   
 }
     
 alert("It took " + totalGuesses + " tries to guess the correct number.");
